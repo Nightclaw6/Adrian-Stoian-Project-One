8e0d46c627d571071b1bb19f0115dc208be5b48e
Nightclaw6 <sclaudiu82@yahoo.com>
2022-02-04 22:51:28 -0500

    added hamburger menu main page first draft

---
+++ menu.js
@@ -0,0 +1,8 @@
+// Variables
+
+const toggleBtn = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+toggleBtn.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+})
